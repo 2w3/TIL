a80db9967b6be6942b7ff22717892fd64145b541
SUNG HO LEE <sungholee@SUNGs-MacBook-Pro.local>
2017-03-10 23:08:36 +0100

    queue.py, stack.py

---
+++ python/queue/queue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+queue = []
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+
+while queue:
+    print( "queue pop :", queue.pop(0) )
---
+++ python/stack/stack.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+
+while stack:
+    print( "stack pop :", stack.pop() )
