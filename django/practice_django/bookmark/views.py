fa99fd2c8de69ae84780d21e8c7bee23091df4b7
SUNG HO LEE <sungholee@SUNGs-MacBook-Pro.local>
2017-03-19 20:48:59 +0100

    django bookmark practice

---
+++ django/practice_django/bookmark/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+# ListView
+class BookmarkLV(ListView):
+    model = Bookmark
+
+# DetailView
+class BookmarkDV(DetailView):
+    model = Bookmark
